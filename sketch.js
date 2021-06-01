9e0429b2a5928c1aadb725ecf8fe202b8d8a10c8
mrsneaky29 <84763159+mrsneaky29@users.noreply.github.com>
2021-06-01 16:18:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var s1,s2,s3,s4;
+var sprites =[]
+
+function setup() {
+  createCanvas(400, 400);
+  s1 = createSprite(75, 100, 30, 30);
+  s2 = createSprite(150, 250, 30, 30);
+  s3 = createSprite(300, 300, 30, 30);
+  s4 = createSprite(350, 150, 30, 30);
+  
+  sprites = [s1,s2,s3,s4]; 
+  for(var i =0;i<sprites.length;i++){
+    console.log(sprites[i].position.x)
+  }
+ 
+}
+function draw() {
+
+  background(150,20,200);
+  drawSprites();
+}
+
+
+
+
